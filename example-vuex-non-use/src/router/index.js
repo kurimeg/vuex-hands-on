6d1347224631105516911ae781c7606e6f6f2eab
Megumi Kurihara <pomme.c5.musique@gmail.com>
2018-07-31 01:18:54 +0900

    ルーティングの変更

--- example-vuex-non-use/src/router/index.js
+++ example-vuex-non-use/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import SeatMap from '@/pages/SeatMap'
+import Index from '@/pages/Root/Index'
 
 Vue.use(Router)
 
@@ -8,8 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'SeatMap',
-      component: SeatMap
+      component: Index
     }
   ]
 })
