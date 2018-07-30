44b6227520b0d04ccb3e166099e2ef6758f21458
Megumi Kurihara <pomme.c5.musique@gmail.com>
2018-07-30 21:30:07 +0900

    HTTPクライアントの修正

--- handson/src/utils/clientAPI.js
+++ handson/src/utils/clientAPI.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import * as messages from '@/assets/messages'
 
 const client = axios.create({
   baseURL: process.env.API_BASE_URL
@@ -8,18 +7,14 @@ export default (Vue, { store }) => {
   client.interceptors.request.use((config) => {
     return config
   }, (error) => {
-    store.commit('components/globals/snackbar/error', messages.E_001)
+    console.log(error)
     return Promise.reject(error)
   })
 
   client.interceptors.response.use((response) => {
     return Promise.resolve(response.data)
   }, (error) => {
-    if (error.response) {
-      store.commit('components/globals/snackbar/error', error.response.data.status.message)
-    } else {
-      store.commit('components/globals/snackbar/error', messages.E_001)
-    }
+    console.log(error)
     return Promise.reject(error)
   })
 
